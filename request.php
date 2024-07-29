02a052ba98f1876223f4fbf48d428825f9572c05
Abdyushev Rafail <Abdyushev.R@romir.ru>
2024-07-29 17:50:57 +0300

    request.php

--- request.php
+++ request.php
@@ -1,11 +1,27 @@
 <?php
     $body2 = file_get_contents('php://input');
     $data = json_decode($body2);
-    $url= "https://rn.imystery.ru/mlog/send_log_" . strval( $data->date ) . ".txt";
+    $urlString = '';
+    $input = fopen("config.txt", "r");
+    while(!feof($input)) { 
+        $dataDB[] = trim(fgets($input));
+    }
+    switch ($data->project) {
+        case "Мегафон":
+            $urlString = $dataDB[0];
+            break;
+        case "Магнит":
+            $urlString = $dataDB[1];
+            break;
+        case "Роснефть":
+            $urlString = $dataDB[2];
+            break;
+    }
+    
+    $urlString .= strval( $data->date ) . ".txt";
 //    $response = file_get_contents($url);
 
-    
-    if (($response = @file_get_contents($url)) === false) {
+    if (($response = @file_get_contents($urlString)) === false) {
         $error = error_get_last();
         $response = "no data";
     } 
